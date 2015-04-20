096cd8bab61ace5dcc418fbc817ad55c14194275
Nigel Gilbert <ngilbert@smu.edu>
2015-04-19 23:17:35 -0500

    Changed to c++11, added custom itoa function for compiler issues

---
+++ itoa.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+#include <algorithm>
+
+// Citation: http://www.jb.man.ac.uk/~slowe/cpp/itoa.html
+char* itoa( int value, char* result, int base ) {
+
+    // check that the base if valid
+
+    if (base < 2 || base > 16) { *result = 0; return result; }
+
+
+
+    char* out = result;
+
+    int quotient = value;
+
+
+
+    do {
+
+        *out = "0123456789abcdef"[ std::abs( quotient % base ) ];
+
+        ++out;
+
+        quotient /= base;
+
+    } while ( quotient );
+
+
+
+    // Only apply negative sign for base 10
+
+    if ( value < 0 && base == 10) *out++ = '-';
+
+
+
+    std::reverse( result, out );
+
+    *out = 0;
+
+    return result;
+
+}
--- mesh.h
+++ mesh.h
@@ -2,12 +2,24 @@
 #define MESH_H
 
 #include <vector>
+#include <unordered_map>
 
 #include "ip.h"
 
+namespace std
+{
+    template <>
+    struct hash<Ip>
+    {
+        size_t operator()(const Ip& ip) const
+        {
+            return (hash<int>()(ip.getIpNum(3)) ^ (hash<int>()(ip.getIpNum(4) << 1)) >> 1);
+        }
+    };
+}
+
 class Mesh
 {
-
 private:
     struct node {
         std::vector<node*> connectedNodes;
@@ -19,9 +31,8 @@ private:
         std::string data;
     };
 
-    // For grid - 2D array of nodes
-    std::vector<node> nodeGrid[][];
-    // For mesh - hash table
+    std::vector<std::vector<node>> nodeGrid;
+    std::unordered_map<Ip, node> nodeMap;
 
 public:
     Mesh();
