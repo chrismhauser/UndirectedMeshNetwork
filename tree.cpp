997a27a62d353a75bad2c06b061806cf1ef7b0af
chrismhauser <chrismhauser@yahoo.com>
2015-04-29 18:42:54 -0500

    fixed findNode, added findPath

--- tree.cpp
+++ tree.cpp
@@ -23,6 +23,9 @@ tree::tree(Ip ip)
 
 tree::node* tree::findNode(Ip ip)
 {
+    // TEST STATEMENT
+    std::cout << "findNode()" << std::endl;
+
     node* parent = head;
     return findNode(ip, parent);
 }
@@ -35,51 +38,67 @@ tree::node* tree::findNode(Ip ip, node* parent)
         if (parent->children.at(i)->address == ip)
             return parent->children.at(i);
 
-        findNode(ip, parent->children.at(i));
+        node* temp = findNode(ip, parent->children.at(i));
+        if(temp != nullptr) {
+            return temp;
+        }
     }
     // if not found return nullptr
     return nullptr;
 }
 
-// Used in conjunction with findNode to push children to the appropriate parent
-void tree::insertChild(node *parent, Ip ip, short connectionWeight)
+std::queue<Ip> tree::findPath(Ip dest)
 {
-    short newWeight = parent->weight + connectionWeight;
+    // TEST STATEMENT
+    std::cout << "findPath()" << std::endl;
 
-    // first see if the node you want to add is already in the tree
-    node* check = findNode(ip);
-    if (check != nullptr) {
+    node* parent = head;
+    std::stack<Ip> s;
+    s.push(parent->address);
+    s = findPath(dest, parent, s);
 
-        // return if ip exists and its weight is less than the new path
-        if (check->weight <= newWeight)
-            return;
+    std::stack<Ip> buffer;
+    std::queue<Ip> path_q;
+    Ip temp;
 
-        // if it's weight is larger, then move it around to the shorter path
-        else {
-            // first remove the node from it's parent's children
-            // note: iterator traversal is required here for using erase() method
-            auto it = check->parent->children.begin();
-            for (; it != check->parent->children.end(); it++) {
-
-                if ((*it)->address == ip)
-                   check->parent->children.erase(it);
-             }
-
-             // assign new values for the moved node
-             check->parent = parent;
-             check->weight = newWeight;
-             parent->children.push_back(check);
-             return;
-        }
+    while(!s.empty())
+    {
+        temp = s.top();
+        s.pop();
+        buffer.push(temp);
     }
 
-    // if it's not already in the tree, then add it
-    node* child = new node;
-    child->address = ip;
-    child->parent = parent;
-    child->weight = newWeight;
-    parent->children.push_back(child);
-    ptrVect.push_back(child);
+    while(!buffer.empty())
+    {
+        temp = buffer.top();
+        buffer.pop();
+        path_q.push(temp);
+    }
+
+    std::queue<Ip> qTemp = path_q;
+
+    return path_q;
+}
+
+// Find function for recursive treversal
+std::stack<Ip> tree::findPath(Ip dest, node* parent, std::stack<Ip> s)
+{
+    for (size_t i = 0; i < parent->children.size(); ++i) {
+        s.push(parent->children.at(i)->address);
+
+        if (parent->children.at(i)->address == dest) {
+            //s.push(parent->children.at(i)->address);
+            return s;
+        }
+
+        s = findPath(dest, parent->children.at(i), s);
+        if(s.top() == dest) {
+            return s;
+        }
+        s.pop();
+    }
+    // if not found return nullptr
+    return s;
 }
 
 tree::~tree()
--- tree.h
+++ tree.h
@@ -1,6 +1,8 @@
 #ifndef TREE_H
 #define TREE_H
 #include <vector>
+#include <queue>
+#include <stack>
 #include "ip.h"
 #include "iostream"
 
@@ -20,6 +22,8 @@ public:
     ~tree();
     node* findNode(Ip a);
     node* findNode(Ip a, node* parent);
+    std::queue<Ip> findPath(Ip dest);
+    std::stack<Ip> findPath(Ip dest, node* parent, std::stack<Ip> s);
     void insertChild(node *parent, Ip ip, short connectionWeight);
 
 private:
